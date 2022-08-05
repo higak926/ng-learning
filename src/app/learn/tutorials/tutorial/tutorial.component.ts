d13ee9c1e3728771df378c191e3bf40db9f52730
kazuakihiga <higak926@gmail.com>
2022-08-06 00:57:51 +0900

    design adjust

--- src/app/learn/tutorials/tutorial/tutorial.component.ts
+++ src/app/learn/tutorials/tutorial/tutorial.component.ts
@@ -3,13 +3,12 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-tutorial',
   templateUrl: './tutorial.component.html',
-  styleUrls: ['./tutorial.component.scss']
+  styleUrls: ['./tutorial.component.scss'],
 })
 export class TutorialComponent implements OnInit {
+  tutorialImgSrc: string = 'assets/images/tutorial_image.png';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
