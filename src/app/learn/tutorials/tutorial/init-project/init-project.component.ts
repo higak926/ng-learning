53026b6555a233331ffeb6b41200690a8004bfb7
kazuakihiga <higak926@gmail.com>
2022-08-20 02:53:34 +0900

    tutorial init project

--- src/app/learn/tutorials/tutorial/init-project/init-project.component.ts
+++ src/app/learn/tutorials/tutorial/init-project/init-project.component.ts
@@ -3,13 +3,22 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-init-project',
   templateUrl: './init-project.component.html',
-  styleUrls: ['./init-project.component.scss']
+  styleUrls: ['./init-project.component.scss'],
 })
 export class InitProjectComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  selectInitConfigImg: string =
+    'assets/images/tutorial/init_project/select_init_config.png';
+  createdProjectImg: string =
+    'assets/images/tutorial/init_project/created_project.png';
+  initServeImg: string = 'assets/images/tutorial/init_project/init_serve.png';
+  angularWelcomImg: string =
+    'assets/images/tutorial/init_project/angular_welcom.png';
+  changeComponentImg: string =
+    'assets/images/tutorial/init_project/change_component.png';
+  changeHtmlImg: string = 'assets/images/tutorial/init_project/change_html.png';
+  babyInitImg: string = 'assets/images/tutorial/init_project/baby_init.png';
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
