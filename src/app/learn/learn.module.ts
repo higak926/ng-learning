3a2b4504bda645aebaf60c92484f1a99a35f4221
kazuakihiga <higak926@gmail.com>
2022-08-27 01:24:37 +0900

    add nz-zorro/nz-collapse

--- src/app/learn/learn.module.ts
+++ src/app/learn/learn.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 import { BuiltEnvironmentComponent } from './built-environment/built-environment.component';
 import { TutorialComponent } from './tutorials/tutorial/tutorial.component';
 import { BeforeTutorialComponent } from './before-tutorial/before-tutorial.component';
 import { InitProjectComponent } from './tutorials/tutorial/init-project/init-project.component';
+import { NzCollapseModule } from 'ng-zorro-antd/collapse';
 
 const routes: Routes = [
   {
@@ -32,7 +34,7 @@ const routes: Routes = [
     BeforeTutorialComponent,
     InitProjectComponent,
   ],
-  imports: [RouterModule.forChild(routes)],
+  imports: [RouterModule.forChild(routes), CommonModule, NzCollapseModule],
   providers: [],
 })
 export class LearnModule {}
